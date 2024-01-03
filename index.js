ff272424c3ef9efde53a6886b86e710e36c92551
Raman Gupta <gr.gupta.raman@gmail.com>
2024-01-03 14:13:24 -0500

    Refactor: Fix URL String formatting

--- index.js
+++ index.js
@@ -2,7 +2,22 @@
 const puppeteer = require("puppeteer");
 
 const url =
-  "https://uocampus.public.uottawa.ca/psc/csprpr9pub/EMPLOYEE/SA/c/UO_SR_AA_MODS.UO_PUB_CLSSRCH.GBL?languageCd=ENG&PortalActualURL=https%3a%2f%2fuocampus.public.uottawa.ca%2fpsc%2fcsprpr9pub%2fEMPLOYEE%2fSA%2fc%2fUO_SR_AA_MODS.UO_PUB_CLSSRCH.GBL%3flanguageCd%3dENG&PortalContentURL=https%3a%2f%2fuocampus.public.uottawa.ca%2fpsc%2fcsprpr9pub%2fEMPLOYEE%2fSA%2fc%2fUO_SR_AA_MODS.UO_PUB_CLSSRCH.GBL&PortalContentProvider=SA&PortalCRefLabel=Public%20Class%20Search&PortalRegistryName=EMPLOYEE&PortalServletURI=https%3a%2f%2fuocampus.public.uottawa.ca%2fpsp%2fcsprpr9pub%2f&PortalURI=https%3a%2f%2fuocampus.public.uottawa.ca%2fpsc%2fcsprpr9pub%2f&PortalHostNode=SA&NoCrumbs=yes&PortalKeyStruct=yes";
+  "https://uocampus.public.uottawa.ca/" +
+  "psc/csprpr9pub/EMPLOYEE/SA/c/" +
+  "UO_SR_AA_MODS.UO_PUB_CLSSRCH.GBL?" +
+  "languageCd=ENG&PortalActualURL=https%3a%2f%2f" +
+  "uocampus.public.uottawa.ca%2fpsc%2fcsprpr9pub%2f" +
+  "EMPLOYEE%2fSA%2fc%2fUO_SR_AA_MODS.UO_PUB_CLSSRCH.GBL%3f" +
+  "languageCd%3dENG&PortalContentURL=https%3a%2f%2f" +
+  "uocampus.public.uottawa.ca%2fpsc%2fcsprpr9pub%2f" +
+  "EMPLOYEE%2fSA%2fc%2fUO_SR_AA_MODS.UO_PUB_CLSSRCH.GBL&" +
+  "PortalContentProvider=SA&PortalCRefLabel=" +
+  "Public%20Class%20Search&PortalRegistryName=EMPLOYEE&" +
+  "PortalServletURI=https%3a%2f%2f" +
+  "uocampus.public.uottawa.ca%2fpsp%2fcsprpr9pub%2f&" +
+  "PortalURI=https%3a%2f%2fuocampus.public.uottawa.ca" +
+  "%2fpsc%2fcsprpr9pub%2f&PortalHostNode=SA&" +
+  "NoCrumbs=yes&PortalKeyStruct=yes";
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
