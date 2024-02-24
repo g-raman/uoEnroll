96a1cff3ef4b9b036a2d007e4aa502f560330256
Raman Gupta <gr.gupta.raman@gmail.com>
2024-02-24 12:35:52 -0500

    feat: extend tailwind to use poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
