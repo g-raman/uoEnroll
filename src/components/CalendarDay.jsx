49e7df9c857d0e7c85b87f783fbeba4a60133999
Raman Gupta <gr.gupta.raman@gmail.com>
2024-02-24 14:41:13 -0500

    fix: adjust am/pm for time and include range from 6am-11pm

--- src/components/CalendarDay.jsx
+++ src/components/CalendarDay.jsx
@@ -1,11 +1,15 @@
 const CalendarDay = () => {
   return (
     <div className="flex h-full w-full flex-col overflow-scroll py-12">
-      {Array.from({ length: 12 }).map((_, i) => {
+      {Array.from({ length: 18 }).map((_, i) => {
+        let hour = i + 6;
+        const amOrPm = hour < 12 ? 'AM' : 'PM';
+        hour = hour === 12 ? 12 : hour % 12;
+
         return (
           <div className="flex w-full justify-between" key={i}>
             <span className="w-14 font-poppins text-xs font-light leading-[0] text-gray-400">
-              {(i + 7) % 12 == 0 ? 12 : (i + 7) % 12} AM
+              {`${hour} ${amOrPm}`}
             </span>
             <div className="h-24 w-full border-t-[1px] border-t-gray-400"></div>
           </div>
