33e923d082674df2d48b5fc05c0c58dd3fc948da
Raman Gupta <gr.gupta.raman@gmail.com>
2024-02-24 12:43:09 -0500

    fix: remove test component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
     <div className="h-dvh w-dvw px-6 py-6 font-poppins font-light text-gray-400">
       <DayNavigation />
       <CalendarDay />
-      <div className="bg-red-500">hi</div>
     </div>
   );
 }
